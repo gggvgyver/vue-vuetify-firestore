d6e2af5c16efb3730d993a943ff2e1c83490945c
gggvgyver <gggvgyver@gmail.com>
2018-12-10 11:06:43 +0900

    라우터 링크

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <v-app>
-    <v-navigation-drawer app>네이드로어</v-navigation-drawer>
+    <v-navigation-drawer app>네비드로어
+      <v-btn @click="$router.push('/')">링크-홈</v-btn>
+      <v-btn @click="$router.push('/list')">링크-리스트</v-btn>
+      <v-btn @click="$router.push('/add')">링크-추가</v-btn>
+      <v-btn @click="$router.push({ name: 'list'})">링크-리스트-파라미터이용</v-btn>
+      <router-link :to="{name: 'list'}">링크-리스트-라우터링크이용</router-link>
+      <!-- <v-list-tile router :to="{name: '/'}"> -->
+    </v-navigation-drawer>
     <v-toolbar app>툴바</v-toolbar>
     <v-content>
       <v-container fluid>
@@ -11,3 +18,12 @@
   </v-app>
 </template>
 
+<script>
+export default {
+  methods: {
+    linkTest () {
+      $router.push({ name: 'show', params: { id: 1 }})
+    }
+  }
+}
+</script>
